8195ca97c5232593ddefe380126aacf48dd81606
Sean Krispin <sean.krispin@gmail.com>
2021-11-03 12:39:53 -0500

    Added IsSubscribedToNewsLetter and MembershipType props to Customer and created new MembershipType.cs class

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,9 @@ namespace Vidly.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+		public bool IsSubscribedToNewsLetter { get; set; }
+		//Now we need to associate our customer class with MembershipType
+		public MembershipType MembershipType { get; set; } //This is what we call a navigation property, because it allows us to navigate from one type to another.
+		//These nav props are useful when you want to load an object and its related objects together from the Db (I believe this is eager loading?)
 	}
 }
---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+	public class MembershipType
+	{//IMPORTANT EF NOTES - In EF, every entity must have a key, which will be mapped to PK of the corresponding table in the database. By convention this prop should be called either Id, or the name of the type+Id. Lesson 26!!!
+		public byte Id { get; set; }
+		public short SignUpFee { get; set; }
+		public byte DurationInMonths { get; set; }
+		public byte DiscountRate { get; set; }
+
+	}
+}
