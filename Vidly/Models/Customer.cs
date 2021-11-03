32b0c23d83682dbc15b76269ced7153e1120d665
Sean Krispin <sean.krispin@gmail.com>
2021-11-03 13:06:43 -0500

    Added data annotations to Customer.Name

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
 	public class Customer
 	{
 		public int Id { get; set; }
+		[Required] //Overriding Conventions - lesson 28
+		[StringLength(255)]
 		public string Name { get; set; }
 		public bool IsSubscribedToNewsLetter { get; set; }
 		//Now we need to associate our customer class with MembershipType
