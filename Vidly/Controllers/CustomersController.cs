5fa73e6c799ebff283015260311cae61bac79b7f
Sean Krispin <sean.krispin@gmail.com>
2021-11-03 20:16:53 -0500

    Seeded db with customers. Added eager loading to Customers.Index action in order to load MembershipType values.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,35 +4,66 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
-        public ActionResult Index()
+		//First off we need a Db context to access the database - LESSON 29
+		private ApplicationDbContext _context { get; set; }
+
+		//Initialize the DbContext. 
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		//This DbCOntext is a disposable object so we need to properly dispose of it.LESSON 29.
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
+		// GET: Customers
+		public ActionResult Index()
         {
-			//hardcode our first set of customers for non-API/non-db controller
-			var customers = new List<Customer>
+			////hardcode our first set of customers for non-API/non-db controller
+			//var customers = new List<Customer>
+			//{
+			//	new Customer {Name = "Sean Krispin", Id = 1},
+			//	new Customer {Name = "Megan Grable", Id = 2}
+			//};
+
+			//Lesson 30 - we used the include method below for EAGER LOADING of the membership types. This is because our Customer class is associated with the MembershipType class via a Customer property of type Membershiptype. In order to use this method, we had to add the Data.Entity namespace
+			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+			if(customers == null)
 			{
-				new Customer {Name = "Sean Krispin", Id = 1},
-				new Customer {Name = "Megan Grable", Id = 2}
-			};
+				return HttpNotFound();
+			}
 
 			//Not passing in/creating a customer viewModel so no need here
 
 			return View(customers);
         }
 
+		//See lesson 30 regarding eager loading. BY DEFAULT ENTITY FRAMEWORK ONLY LOADS THE CUSTOMER OBJECT (or the object passed into the model) AND NOT ITS RELATED OBJECTS/CLASSES
+		//To solve this problem, we need to load the customers and their membership types together. This is called eager loading!!!
 		public ActionResult Details(int id)
 		{
-			var customers = new List<Customer>
-			{
-				new Customer {Name = "Sean Krispin", Id = 1},
-				new Customer {Name = "Megan Grable", Id = 2}
-			};
+			//var customers = new List<Customer //Original hardcoded without Db
+			//{
+			//	new Customer {Name = "Sean Krispin", Id = 1},
+			//	new Customer {Name = "Megan Grable", Id = 2}
+			//};
 
-			var customer = customers.Find(c => c.Id == id);
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+			if(customer == null)
+			{
+				return HttpNotFound();
+			}
 
 			return View(customer);
 		}
