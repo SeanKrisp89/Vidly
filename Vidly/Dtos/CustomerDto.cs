a8de72df2e678ff76164db9ab640f71502f30d7c
Sean Krispin <sean.krispin@gmail.com>
2021-11-09 14:31:49 -0600

    Added MappingProfile and AUtoMapper package. Updated controller return types & args to Dtos and mapped them to their database counterparts. Lastly, updated WebApiConfig to return JSON in camelCase format.

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+	public class CustomerDto
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Please enter the customer's name.")] //Overriding Conventions - lesson 28
+		[StringLength(255)]
+		public string Name { get; set; }
+
+		public bool IsSubscribedToNewsLetter { get; set; }
+
+		//public MembershipType MembershipType { get; set; } We want to remove MembershipType because it is a part of our domain, which creates a dependency for our Dto to our domain. Don't want domain dependencies!
+														   
+		public byte MembershipTypeId { get; set; }
+
+		[Min18YearsIfAMember]
+		public DateTime? Birthdate { get; set; }
+	}
+}
