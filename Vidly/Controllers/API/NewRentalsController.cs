1a88dbc10170fc9f57b6c2ea671389db1c1623ee
Sean Krispin <sean.krispin@gmail.com>
2021-11-12 14:12:28 -0600

    Added new controller and Dto for rentals.

---
+++ Vidly/Controllers/API/NewRentalsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.API
+{
+    public class NewRentalsController : ApiController
+    {
+        // GET: NewRentals
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vidly/Dtos/NewRentalDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+using AutoMapper;
+
+namespace Vidly.Dtos
+{
+	public class NewRentalDto
+	{
+		public int CustomerId { get; set; }
+		public List<int> MoviesIds { get; set; }
+	}
+}
